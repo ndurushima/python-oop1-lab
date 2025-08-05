695e4642aba4cf70057e5bb76fb87a79969c31aa
ndurushima <ndu1994@gmail.com>
2025-08-04 21:25:24 -0500

    try again

--- lib/book.py
+++ lib/book.py
@@ -3,13 +3,14 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
+        self._page_count = None
         self.page_count = page_count
     
     def page_count(self):
         return self._page_count
-    
-    def page_count(self, value = int):
-        if not int:
+
+    def page_count(self, value):
+        if not isinstance(value, int):
             print("page_count must be an integer")
         else:
             self._page_count = value
