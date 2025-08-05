3b6a5a29268f371b224da63a5de82c4965859785
ndurushima <ndu1994@gmail.com>
2025-08-04 21:58:30 -0500

    fixes to coffee.py

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    def __init__(self, size, price = ("Small", "Medium", "Large")):
+    def __init__(self, size, price):
+        self._size = None
         self.size = size
         self.price = price
-    
-    def get_size(self):
+
+    def size(self):
         return self._size
-    
-    def set_size(self, value):
+
+    def size(self, value):
         if value not in ("Small", "Medium", "Large"):
             print("size must be Small, Medium, or Large")
         else:
             self._size = value
-    
+
     def tip(self):
-        print("This coffee is great, here`s a tip!")
+        print("This coffee is great, here’s a tip!")
         self.price += 1
+
